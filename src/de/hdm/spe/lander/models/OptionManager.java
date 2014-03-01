fec36a3ed4724090ac92d5b8bf8c67dfb57cb16e
bboyarskiy <boris@10.60.35.76>
2014-03-01 15:27:14 +0100

    Optionennenenenenenenen

--- src/de/hdm/spe/lander/models/OptionManager.java
+++ src/de/hdm/spe/lander/models/OptionManager.java
@@ -1,43 +1,69 @@
 package de.hdm.spe.lander.models;
 
+import de.hdm.spe.lander.statics.Static;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
 public class OptionManager {
 	
 	public String[] options;
-	private boolean mState = true;
-	private boolean sState = true;
+	private boolean musicState = true;
+	// true  = DE
+	private boolean languageState = true;
+	private Context mContext;
 	
-	public OptionManager(){
-		
+	public OptionManager(Context context){
+		mContext = context;
 	}
 	
 	public void loadOptions(){
+		
+		SharedPreferences sharedPre = mContext.getSharedPreferences(Static.sSettingsPrefsName, 0);
+		musicState = sharedPre.getBoolean(Static.sSettingsMusic, true);
+		languageState = sharedPre.getBoolean(Static.sSettingsLanguage, true);
+		
 		options = new String[5];
 		
-			options[0] = "Music ON";
+		String musicState = this.musicState?"ON":"OFF";
+		String langString = this.languageState?"DE":"EN";
+		
+			options[0] = "Music "+musicState;
 			options[1] = "Highscore";
 			options[2] = "Schwierigkeit";
-			options[3] = "Sprache DE";
+			options[3] = "Sprache "+langString;
 			options[4] = "Zurück";
 	
 	}
 	
+	 public void saveOptions(){
+		 Editor edit = mContext.getSharedPreferences(Static.sSettingsPrefsName, 0).edit();
+		 edit.putBoolean(Static.sSettingsMusic, musicState);
+		 edit.putBoolean(Static.sSettingsLanguage, languageState);
+		 edit.commit();
+	 }
 	
 	public void changeOptions(int i){
 
-		if(i == 0 && mState == true){
-			options[0] = "Music OFF";
-			mState = false;
-		}else{
-			options[0] = "Music ON";
-			mState = true;
-		}
-		
-		if(i == 3 && sState == true){
-			options[3] = "Sprache EN";
-			sState = false;
-		}else{
-			options[3] = "Sprache DE";
-			sState = true;
+		switch (i){
+		case 0:
+			if(musicState){
+				options[0] = "Music OFF";
+				musicState = false;
+			}else{
+				options[0] = "Music ON";
+				musicState = true;
+			}
+			break;
+		case 3:
+			if(languageState){
+				options[3] = "Sprache EN";
+				languageState = false;
+			}else{
+				options[3] = "Sprache DE";
+				languageState = true;
+			}
+			break;
 		}
 				
 	}
